539d02f9182246fffdfc90ed4e8236ece952d78c
Tim Green <tgreen@opscode.com>
2012-07-10 18:34:02 -0500

    updated installed_software typo

--- win32_software.rb
+++ win32_software.rb
@@ -7,7 +7,7 @@ installed_software Mash.new
 apps = WMI::Win32_Product.find(:all)
 
 apps.each do |app|
-  installed_softwarestalled_software[app.packagename] = Mash.new
+  installed_software[app.packagename] = Mash.new
   %w(name version vendor packagenameckagename installdate).each do |attrib|
     installed_software[app.packagenameename][attrib] = app.send(attrib)
   end
